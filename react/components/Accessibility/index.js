ba7271766d8d784c5e6d6e0f5ce6dc25f9a6b108
Gustavo Santana <gustavo_santana_jussi@whirlpool.com>
2021-12-23 15:01:39 -0300

    fix typo in accessibility

--- react/components/Accessibility/index.js
+++ react/components/Accessibility/index.js
@@ -5,13 +5,13 @@ import "./styles.global.css";
 const Accessibility = () => {
   const zoomOptions = [0, 10, 20, 30];
 
-  const [accessebility, setAccessibility] = useState({
+  const [accessibility, setAccessibility] = useState({
     zoom: 0,
     grayscale: false,
   });
 
   const getFromLocalStorage = () => {
-    const storage = localStorage.getItem("accessebility");
+    const storage = localStorage.getItem("accessibility");
 
     if (storage) {
       setAccessibility(JSON.parse(storage));
@@ -19,11 +19,11 @@ const Accessibility = () => {
   };
 
   const updateLocalStorage = () => {
-    localStorage.setItem("accessebility", JSON.stringify(accessebility));
+    localStorage.setItem("accessibility", JSON.stringify(accessibility));
   };
 
   const updateBodyAttributes = () => {
-    const arrJSON = Object.entries(accessebility);
+    const arrJSON = Object.entries(accessibility);
 
     arrJSON.forEach((item) => {
       document.body.setAttribute(`data-${item[0]}`, item[1]);
@@ -37,7 +37,7 @@ const Accessibility = () => {
   useEffect(() => {
     updateLocalStorage();
     updateBodyAttributes();
-  }, [accessebility]);
+  }, [accessibility]);
 
   return (
     <div className="accessibility">
@@ -48,9 +48,9 @@ const Accessibility = () => {
           <select
             id="zoom"
             name="zoom"
-            value={accessebility.zoom}
+            value={accessibility.zoom}
             onChange={(e) => {
-              setAccessibility({ ...accessebility, zoom: e.target.value });
+              setAccessibility({ ...accessibility, zoom: e.target.value });
             }}
           >
             {zoomOptions.map((item, index) => {
@@ -69,12 +69,12 @@ const Accessibility = () => {
         <span className="accessibility__text">Escala de cinza</span>
         <div
           className={`accessibility__toggle ${
-            accessebility.grayscale ? "accessibility__toggle--active" : ""
+            accessibility.grayscale ? "accessibility__toggle--active" : ""
           }`}
           onClick={() =>
             setAccessibility({
-              ...accessebility,
-              grayscale: !accessebility.grayscale,
+              ...accessibility,
+              grayscale: !accessibility.grayscale,
             })
           }
         >
